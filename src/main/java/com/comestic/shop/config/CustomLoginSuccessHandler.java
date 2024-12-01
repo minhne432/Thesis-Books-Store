d9689217b3d6a07a6371f45bd0e99c45c9128518
minh <nh.minh0403@gmail.com>
2024-12-01 20:16:08 +0700

    chinh sua phan quyen

--- src/main/java/com/comestic/shop/config/CustomLoginSuccessHandler.java
+++ src/main/java/com/comestic/shop/config/CustomLoginSuccessHandler.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Set;
 
 @Component
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
@@ -17,8 +18,14 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
 
-        // Check user roles and redirect accordingly
-        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+        // Danh sách các vai trò chuyển hướng đến cùng một trang
+        Set<String> branchRoles = Set.of("ROLE_ADMIN", "ROLE_BRANCH_MANAGER");
+
+        // Kiểm tra nếu user có bất kỳ vai trò nào trong branchRoles
+        boolean isBranchRole = authentication.getAuthorities().stream()
+                .anyMatch(authority -> branchRoles.contains(authority.getAuthority()));
+
+        if (isBranchRole) {
             response.sendRedirect("/branches");
         } else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"))) {
             response.sendRedirect("/shop/products/list");
@@ -26,4 +33,5 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
             response.sendRedirect("/login?error=true"); // fallback if role not matched
         }
     }
+
 }
