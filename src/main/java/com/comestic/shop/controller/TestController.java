08d924fb2c45c6a1c13eb2743c19489eb1facece
minh <nh.minh0403@gmail.com>
2024-12-08 05:36:36 +0700

    test list customer's address

--- src/main/java/com/comestic/shop/controller/TestController.java
+++ src/main/java/com/comestic/shop/controller/TestController.java
@@ -1,7 +1,14 @@
 package com.comestic.shop.controller;
 
 
+import com.comestic.shop.model.Address;
+import com.comestic.shop.model.Customer;
+import com.comestic.shop.model.CustomerAddress;
+import com.comestic.shop.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +20,8 @@ import java.util.List;
 @RequestMapping("/test")
 public class TestController {
 
+    @Autowired
+    private CustomerService customerService;
 
     static class Greeting {
         private String message;
@@ -55,5 +64,20 @@ public class TestController {
     @RequestMapping("/sayHelloPage")
     public String sayHelloPage(){
         return "test/greetings";
+    }
+
+
+    private Customer getCurrentCustomer() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return customerService.getCustomerByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Khách hàng không tồn tại"));
+    }
+
+    @GetMapping("/testAddress")
+    public String listAddress(Model model){
+        Customer customers = getCurrentCustomer();
+        List<CustomerAddress> customerAddresses = customers.getCustomerAddresses();
+        model.addAttribute("customerAddresses",customerAddresses);
+        return "test/test_address";
     }
 }
